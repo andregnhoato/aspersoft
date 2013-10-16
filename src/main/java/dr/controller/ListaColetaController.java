8179bee2abf1ad45927148e5350581174c677120
Andre Gnhoato <andregnhoato@gmail.com>
2013-10-16 17:49:18 -0300

    Limpeza de código

--- src/main/java/dr/controller/ListaColetaController.java
+++ src/main/java/dr/controller/ListaColetaController.java
@@ -1,10 +1,7 @@
 package dr.controller;
 
-import dr.event.AbstractEventListener;
-import dr.event.ensaio.AtualizaColetaEvent;
 import dr.model.Ensaio;
 import dr.ui.coleta.ColetaListView;
-import javafx.event.EventType;
 
 /**
  * Define a <code>Controller</code> principal do sistema, responsável por gerir a tela com a lista de <code>Coleta</code>.
@@ -18,18 +15,7 @@ public class ListaColetaController extends PersistenceController {
     private ColetaListView coletaView;
 
     public ListaColetaController(AbstractController parent) {
-        this.coletaView = new ColetaListView();
-        
-        //registerAction(coletaView.getTable().get, null);
-                
-        registerEventListener(AtualizaColetaEvent.class, new AbstractEventListener<AtualizaColetaEvent>(){
-            @Override
-            public void handleEvent(AtualizaColetaEvent event){
-                System.out.println("**** Evento de saída da celula");
-            }
-        });
-        
-        
+        this.coletaView = new ColetaListView();       
     }
 
     void setEnsaio(Ensaio e) {
