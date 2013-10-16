b55c41712bfaf300d2112a877a569ed140701fc0
Andre Gnhoato <andregnhoato@gmail.com>
2013-10-16 17:51:18 -0300

    Issue #21 - lógica de sopreposição

--- src/main/java/dr/test/PersistenceTest.java
+++ src/main/java/dr/test/PersistenceTest.java
@@ -11,25 +11,26 @@ import dr.dao.EnsaioDAO;
 import dr.dao.EnsaioDAOImpl;
 import dr.model.*;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  *
  * @author andre
  */
 public class PersistenceTest extends PersistenceController {
-    
+
     public static ColetaDAO cdao;
     public static EnsaioDAO edao;
-    
-    public static void main(String[] args) throws Exception{
-        
+
+    public static void main(String[] args) throws Exception {
+
         PersistenceTest pt = new PersistenceTest();
 //        pt.listarColetasByEnsaio(1);
 //        pt.qtdeColetasByEnsaio(1);
@@ -37,46 +38,45 @@ public class PersistenceTest extends PersistenceController {
 //        pt.qtdeColetasByEnsaio(1);
         //pt.qtdeEnsaios();
         //pt.sobreposicao(4, 12, 12);
-        for (int i = 0; i < 108; i++) {
-            pt.qtdeColetasByEnsaio(i);
-            
-        }
 
-       
-        
-       
+        pt.qtdeColetas();
+
+
+
+
+
     }
-    
-    public void atualizarColeta(int idEnsaio, int linha, int coluna){
+
+    public void atualizarColeta(int idEnsaio, int linha, int coluna) {
         Ensaio e;
-        
-        try{
+
+        try {
             e = edao.findById(idEnsaio);
             Coleta c = cdao.findColetaByPosicao(e, linha, coluna);
             c.setValor(8F);
             cdao.update(c);
-        }catch(Exception ex){
+        } catch (Exception ex) {
             Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     //remove as coletas de um determinado ensaio param id do ensaio
-    public void removeColetasByEnsaio(int idEnsaio){
+    public void removeColetasByEnsaio(int idEnsaio) {
         Ensaio e = new Ensaio();
         e.setId(idEnsaio);
         List<Coleta> coletas;
         try {
             coletas = (List<Coleta>) cdao.findColetasByEnsaio(e);
             for (Iterator<Coleta> it = coletas.iterator(); it.hasNext();) {
-            Coleta coleta = it.next();
-            System.out.println(cdao.remove(coleta));
+                Coleta coleta = it.next();
+                System.out.println(cdao.remove(coleta));
             }
         } catch (Exception ex) {
             Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void listarColetasByEnsaio(int idEnsaio){
+
+    public void listarColetasByEnsaio(int idEnsaio) {
         try {
             Ensaio e = new Ensaio();
             e.setId(idEnsaio);
@@ -93,125 +93,184 @@ public class PersistenceTest extends PersistenceController {
         } catch (Exception ex) {
             Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
-    public void qtdeColetasByEnsaio(int idEnsaio ){
+
+    public void qtdeColetas() {
+        try {
+
+            List<Coleta> coletas = (List<Coleta>) cdao.findAll();
+
+
+            System.out.println("Quantidade de coletas: " + coletas.size());
+
+
+        } catch (Exception ex) {
+            Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void qtdeColetasByEnsaio(int idEnsaio) {
         try {
             Ensaio e = new Ensaio();
             e.setId(idEnsaio);
             List<Coleta> coletas = (List<Coleta>) cdao.findColetasByEnsaio(e);
 
-            
+
             System.out.println("Quantidade de coletas: " + coletas.size());
-            
-            if(coletas.size()<256)
-                throw new Exception("coletas zuadas no ensaios"+coletas.get(0).getEnsaio().getDescricao());
-            
+
+            if (coletas.size() < 256) {
+                throw new Exception("coletas zuadas no ensaios" + coletas.get(0).getEnsaio().getDescricao());
+            }
+
         } catch (Exception ex) {
             Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
+
     }
-    
-    public void qtdeEnsaios(){
+
+    public void qtdeEnsaios() {
         try {
-            List<Ensaio> ensaios = (List<Ensaio>) edao.findAll();            
+            List<Ensaio> ensaios = (List<Ensaio>) edao.findAll();
             System.out.println("Quantidade de ensaios: " + ensaios.size());
-            
+
         } catch (Exception ex) {
             Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
+
     }
-    
-    public void sobreposicao(int idEnsaio, int espacamentoX, int espacamentoY ){
+
+    public void sobreposicao(int idEnsaio, int espacamentoX, int espacamentoY) {
         try {
             Ensaio e = edao.findById(idEnsaio);
             DecimalFormat df = new DecimalFormat("0.00");
-            float sobreposicao = 0;
+
+            float sobreposicaoX = 0;
+            float sobreposicaoY = 0;
             boolean perfeito = false;
-            if(espacamentoX == espacamentoY){
-                sobreposicao = espacamentoX / e.getEspacamentoPluviometro();
-            }
+
+
             //coletas
             List<Coleta> coletas = (List<Coleta>) cdao.findColetasByEnsaio(e);
-            if(coletas.size()<(e.getGridAltura()*e.getGridLargura())){
+            if (coletas.size() < (e.getGridAltura() * e.getGridLargura())) {
                 throw new Exception("Invalid length array");
             }
-            
+
+
             //1 identificando a localização do aspersor ** verificar com Paulo
-            int posAspersor = e.getGridAltura() / 2;
+            int posAspersorX = e.getGridAltura() / 2;
+            int posAspersorY = e.getGridLargura() / 2;
+
             //2 identificando se a sobreposição compreende 1/4 do grid
-            if(posAspersor == sobreposicao)
-                perfeito = true;
-            
+            sobreposicaoX = espacamentoX / e.getEspacamentoPluviometro();
+            sobreposicaoY = espacamentoY / e.getEspacamentoPluviometro();
+
+            if (posAspersorX == posAspersorY) {
+                if (posAspersorX == sobreposicaoX && posAspersorY == sobreposicaoY) {
+                    perfeito = true;
+                }
+            }
+
             //armazenar os valores sobrepostos em coletas
 //            ArrayList<Coleta> sobreposicoes  = new ArrayList();
             //3 se sobreposição é perfeita separar os quadrantes para realizar a soma
-            if (perfeito){
+            if (perfeito) {
                 LinkedList<Float> quad1 = new LinkedList<>();
                 LinkedList<Float> quad2 = new LinkedList<>();
                 LinkedList<Float> quad3 = new LinkedList<>();
                 LinkedList<Float> quad4 = new LinkedList<>();
                 LinkedList<Float> sobreposicoes = new LinkedList<>();
-                
-                Iterator i = coletas.iterator();
-                while(i.hasNext()){
-                    Coleta c =(Coleta) i.next();
-                    if(c.getLinha()<posAspersor && c.getColuna() < posAspersor)
+
+
+                for (Coleta c : coletas) {
+                    if (c.getLinha() < posAspersorX && c.getColuna() < posAspersorX) {
                         quad1.add(c.getValor());
-                    else{
-                        if(c.getLinha() < posAspersor && c.getColuna() >= posAspersor)
+                    } else {
+                        if (c.getLinha() < posAspersorX && c.getColuna() >= posAspersorX) {
                             quad2.add(c.getValor());
-                        else{
-                            if(c.getLinha() >= posAspersor && c.getColuna() < posAspersor)
+                        } else {
+                            if (c.getLinha() >= posAspersorX && c.getColuna() < posAspersorX) {
                                 quad3.add(c.getValor());
-                            else{
+                            } else {
                                 quad4.add(c.getValor());
                             }
                         }
                     }
                 }
-                for (int j = 0; j < (sobreposicao*sobreposicao); j++) {
+                for (int j = 0; j < (sobreposicaoX * sobreposicaoY); j++) {
                     Float soma = 0F;
-                    soma = quad1.get(j)+quad2.get(j)+quad3.get(j)+quad4.get(j);
+                    soma = quad1.get(j) + quad2.get(j) + quad3.get(j) + quad4.get(j);
                     sobreposicoes.add(soma);
                 }
-                
-                System.out.println("Quad1: "+ quad1.toString());
-                System.out.println("Quad1 size: "+ quad1.size());
-                System.out.println("Quad2: "+ quad2.toString());
-                System.out.println("Quad2 size: "+ quad2.size());
-                System.out.println("Quad3: "+ quad3.toString());
-                System.out.println("Quad3 size: "+ quad3.size());
-                System.out.println("Quad4: "+ quad4.toString());
-                System.out.println("Quad4 size: "+ quad4.size());
-                System.out.println("VALORES SOBREPOSTOS: "+ sobreposicoes.toString());
-                
-                
-                
-                
-            }else{
+
+                System.out.println("Quad1: " + quad1.toString());
+                System.out.println("Quad1 size: " + quad1.size());
+                System.out.println("Quad2: " + quad2.toString());
+                System.out.println("Quad2 size: " + quad2.size());
+                System.out.println("Quad3: " + quad3.toString());
+                System.out.println("Quad3 size: " + quad3.size());
+                System.out.println("Quad4: " + quad4.toString());
+                System.out.println("Quad4 size: " + quad4.size());
+                System.out.println("VALORES SOBREPOSTOS: " + sobreposicoes.toString());
+
+
+
+
+            } else {
                 //4 sobreposição imperfeita separar os arrays adicionando zero nos espaços a serem completados
-                
+                LinkedList<Float> quad1 = new LinkedList<>();
+                LinkedList<Float> quad2 = new LinkedList<>();
+                LinkedList<Float> quad3 = new LinkedList<>();
+                LinkedList<Float> quad4 = new LinkedList<>();
+                LinkedList<Float> sobreposicoes = new LinkedList<>();
+                Map<Integer, Integer> celulas = new HashMap<>();
+
+
+                for (Coleta c : coletas) {
+                    if (c.getLinha() < posAspersorX && c.getColuna() < posAspersorX) {
+                        quad1.add(c.getValor());
+                        celulas.put(c.getLinha(), c.getColuna());
+                    } else {
+                        if (c.getLinha() < posAspersorX && c.getColuna() >= posAspersorX) {
+                            quad2.add(c.getValor());
+                            celulas.put(c.getLinha(), c.getColuna());
+                        } else {
+                            if (c.getLinha() >= posAspersorX && c.getColuna() < posAspersorX) {
+                                quad3.add(c.getValor());
+                                celulas.put(c.getLinha(), c.getColuna());
+                            } else {
+                                if (c.getLinha() >= posAspersorX && c.getColuna() > posAspersorX) {
+                                    quad4.add(c.getValor());
+                                    celulas.put(c.getLinha(), c.getColuna());
+                                }
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < (sobreposicaoX * sobreposicaoY); j++) {
+                    Float soma = 0F;
+                    soma = quad1.get(j) + quad2.get(j) + quad3.get(j) + quad4.get(j);
+                    sobreposicoes.add(soma);
+                }
+
+
             }
-       
+
 //            System.out.println("grid altura: " + e.getGridAltura() + " grid largura: "+ e.getGridLargura());
 //            System.out.println("espaçamento: " + e.getEspacamentoPluviometro());
 //            System.out.println("Quantidade de coletas: " + coletas.size());
-            
-            
-            
+
+
+
         } catch (Exception e) {
             Logger.getLogger(PersistenceTest.class.getName()).log(Level.SEVERE, null, e);
         }
-        
+
     }
-     
+
     public PersistenceTest() {
         this.cdao = new ColetaDAOImpl(getPersistenceContext());
         this.edao = new EnsaioDAOImpl(getPersistenceContext());
     }
-    
 }
