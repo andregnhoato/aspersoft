92a701953ffc2507b6e7fa97428ebb39dc080208
Andre Gnhoato <andregnhoato@gmail.com>
2013-10-17 17:25:32 -0300

    DateUtil increment

--- src/main/java/dr/util/DateUtil.java
+++ src/main/java/dr/util/DateUtil.java
@@ -4,23 +4,50 @@
  */
 package dr.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *  Util class, turn easy all operations with all kind of date
- *  
+ * Util class, turn easy all operations with all kind of date
+ *
  * @author andregnhoato
  */
-public abstract class DateUtil{
-    
+public abstract class DateUtil {
+
     static Calendar calendar = Calendar.getInstance();
     private static Date utilDate;
-    
+
     /*
      * method that returns the exact time of day at default format
      */
-    public static Date getTime(){
+    public static Date getTime() {
         return calendar.getTime();
+    }
+
+    public static String formatDate(Date data) {
+        if (data != null) {
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            return df.format(data);
+        } else {
+            return null;
+        }
+    }
+
+    public static Date formatDate(String data) {
+        if (data != null && data != "") {
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                return df.parse(data);
+            } catch (ParseException ex) {
+                Logger.getLogger(DateUtil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
+
     }
 }
