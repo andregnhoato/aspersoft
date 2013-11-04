3df8742d9e3a88f84cf38970facdc6f474de447c
Andre Gnhoato <andregnhoato@gmail.com>
2013-11-04 15:53:30 -0200

    ajuste tamanho da tela

--- src/main/java/dr/ui/ensaio/EnsaioTable.java
+++ src/main/java/dr/ui/ensaio/EnsaioTable.java
@@ -4,28 +4,38 @@ import dr.model.Ensaio;
 import java.util.List;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
 
 /**
- * Reune os componentes para formar uma tabela de <code>Ensaio</code>.
- * 
+ * Reune os componentes para formar uma tabela de
+ * <code>Ensaio</code>.
+ *
  * @see ui.EnsaioTableView
- * 
- * @author @Andre
+ *
+ * @author
+ * @Andre
  */
-public class EnsaioTable extends VBox {
-    
+public class EnsaioTable extends BorderPane {
+
     private EnsaioTableView table;
-    
-    public EnsaioTable(){
+
+    public EnsaioTable() {
         table = new EnsaioTableView();
-        table.setMaxSize(890, 490);
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+       
+//        table.setMaxSize(bounds.getWidth() / 2, bounds.getHeight() / 2);
+//        table.setMinSize(bounds.getWidth() / 2.2, bounds.getHeight() / 2.5);
         
-        this.getChildren().addAll(table);
+        
+//        this.getChildren().addAll(table);
+        this.setCenter(table);
         this.setPadding(new Insets(30, 10, 10, 10));//css
     }
-    
+
     public void reload(List<Ensaio> ensaios) {
         table.reload(ensaios);
     }
@@ -33,12 +43,12 @@ public class EnsaioTable extends VBox {
     public void setMouseEvent(EventHandler<MouseEvent> event) {
         table.setOnMouseClicked(event);
     }
-    
+
     public Ensaio getEnsaioSelected() {
         return table.getSelectedItem();
     }
-    
-    public Integer getTotalEnsaio(){
+
+    public Integer getTotalEnsaio() {
         return table.getItems().size();
     }
 }
